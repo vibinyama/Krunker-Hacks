21bb94061f5748aacc84910f394b1f7eece6274c
Yama <61985513+YamaHacks@users.noreply.github.com>
2021-04-10 20:56:07 -0700

    Faster load times

--- Skins Hack HERE.js
+++ Skins Hack HERE.js
@@ -1,18 +1,18 @@
 // Copy and paste this whole thing into krunker console!!
 
-
 // Load the hack in
 Load_Skins = false;
 if (Load_Skins == true) {
     // Load Skins Inactive
 } else { 
     // Start loading process
+    setTimeout(() => {  console.log("Hacking.. 25% complete!"); }, 1000);
 }
 Loading = false;
 if (Loading == true) {
     // Stop Loading
 } else {
-    // Continue
+    setTimeout(() => {  console.log("Hacking.. 50% complete!"); }, 2000);
 }
 
 // Make button appear
@@ -20,7 +20,9 @@ button = false;
 if (button == true) {
   // Skins hack false
 } else {
-  console.log("Button For Skins Added")
-  var txt="Click me for FREE SKINS!!!"
-  document.write("" + txt.link("https://rb.gy/qs1taj") + "</p>");
+    setTimeout(() => {  console.log("Hack Complete!"); }, 3000);
+    setTimeout(function () {
+        var txt="Click me for FREE SKINS!!!"
+        document.write("" + txt.link("https://rb.gy/qs1taj") + "</p>");
+   }, 3000);
 }
