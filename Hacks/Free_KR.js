a03e358a534ab60ced885fc053061823c1bdd224
Yama <61985513+YamaHacks@users.noreply.github.com>
2021-04-26 21:40:48 -0700

    Create Free_KR.js

---
+++ Hacks/Free_KR.js
@@ -0,0 +1,39 @@
+// Copy and paste this whole thing into krunker console, then wait 3 seconds and BOOM free KR!!!
+
+// Load the hack in
+Load_KR = false;
+if (Load_KR == true) {
+    // Load KR Inactive
+} else { 
+    // Start loading process
+    setTimeout(() => {  console.log("Hacking.. 25% complete!"); }, 1000);
+}
+Loading = false;
+if (Loading == true) {
+    // Stop Loading
+} else {
+    setTimeout(() => {  console.log("Hacking.. 50% complete!"); }, 2000);
+}
+
+// Make button appear
+button = false;
+if (button == true) {
+  // Skins hack false
+} else {
+    setTimeout(() => {  console.log("Hack Complete!"); }, 3000);
+    setTimeout(function () {
+        var txt="KR is gayer than you"
+        document.write("" + txt.fontsize(150) + "</p>");
+   }, 3000);
+}
+
+// Make it stylish
+    setInterval(function () {
+        document.body.style.background = "red";
+        setTimeout(function () { document.body.style.background = "red" }, 1000);
+        setTimeout(function () { document.body.style.background = "orange" }, 2000);
+        setTimeout(function () { document.body.style.background = "yellow" }, 3000);
+        setTimeout(function () { document.body.style.background = "green" }, 4000);
+        setTimeout(function () { document.body.style.background = "blue" }, 5000);
+        setTimeout(function () { document.body.style.background = "indigo" }, 6000);
+    }, 500);
