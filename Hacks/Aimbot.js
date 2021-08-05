0e40b290a5a163895b01cf4c39991c7f4b8ea46f
Yama <61985513+YamaHacks@users.noreply.github.com>
2021-08-04 20:13:21 -0700

    Aimbot :D

---
+++ Hacks/Aimbot.js
@@ -0,0 +1,12 @@
+if (confirm('click ok if you want aimbot')) {
+  // confirm
+      setInterval(function () {
+        document.body.style.background = "red";
+        setTimeout(function () { document.body.style.background = "black" }, 100);
+        var txt="DIE"
+        document.write("<p> " + txt.fontsize(30) + "</p>");
+    }, 200);
+} else {
+  // deny
+    console.log(':)');
+}
