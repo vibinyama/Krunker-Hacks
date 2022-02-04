b93ccd4a34def3de79ba15430634258f7eb7ac5a
vibinyama <61985513+vibinyama@users.noreply.github.com>
2022-02-03 21:06:42 -0700

    im gonna kill myself

---
+++ Hacks/NoLag.js
@@ -0,0 +1,32 @@
+// Click f12 to open up chrome console, then paste this into it.
+
+// set it up
+var SetUp=true;
+var setUp=false;
+var SEtup=true;
+var setup=true;
+var SETUP=true;
+var setUP=false;
+var Setup=true;
+
+// run the command
+var True
+quantum=false;
+if (quantum==false){
+  True=false;
+}else if(setUp==true){
+  var bob=true;
+}else{
+  var what=Setup;
+}
+if(what==Setup){
+  var setUP=true;
+}else if(SETUP==true){
+  SEtup=false;
+}
+if (SEtup==false){
+  while(SEtup==false){
+    document.write("setUP=false;")
+  }
+}
+// what the fuck
