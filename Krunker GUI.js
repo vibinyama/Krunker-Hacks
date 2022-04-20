43e36e240a5339fe22cea9e9307143df49ce6e4f
vibinyama <61985513+vibinyama@users.noreply.github.com>
2022-04-20 15:19:34 -0700

    Update Krunker GUI.js

--- Krunker GUI.js
+++ Krunker GUI.js
@@ -45,9 +45,12 @@ function walltrace() {
 }
 
 var krunkerdiv = document.createElement("krunkerdiv");
+krunkerdiv.style.fontSize = "80px";
 krunkerdiv.style.width = "1000px";
 krunkerdiv.style.height = "1000px";
-krunkerdiv.style.background = "green";
+krunkerdiv.style.paddingLeft = "100px";
+krunkerdiv.style.opacity = "0.5";
+krunkerdiv.style.backgroundColor = "green";
 krunkerdiv.style.color = "black";
 krunkerdiv.innerHTML = '<button type="button" onclick="{krunkerdiv.remove();}">Close</button> <button type="button" onclick="{fast();}">Extra Speed</button> <button type="button" onclick="{free()}">Free KR</button> <button type="button" onclick="{freeskin()}">Free Skins</button> <button type="button" onclick="{infammo()}">Infinite Ammo</button> <button type="button" onclick="{nolag()}">No Lag</button> <button type="button" onclick="{walltrace()}">Wall Tracing</button>';
  
