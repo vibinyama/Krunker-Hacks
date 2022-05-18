9d7a33f2c22162a08cc03e11028eadd6637249c9
vibinyama <61985513+vibinyama@users.noreply.github.com>
2022-05-17 23:12:01 -0700

    Create RainbowName.js

---
+++ hacks/RainbowName.js
@@ -0,0 +1,10 @@
+// spin go wheee
+
+(function() {
+    var s = document.createElement('style');
+    s.textContent = '.trololol:hover{-webkit-transform: rotate(180deg);';
+    document.head.appendChild(s);
+    document.body.classList.add('trololol');
+}());
+
+// stolen ngl
